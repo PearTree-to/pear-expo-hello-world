245acb922e06f48a7c5fbb5a466b65ab544aa843
agayen <abhijit@peartree.to>
2024-04-30 11:29:13 +0530

    bare-fs try

--- bare/main.js
+++ bare/main.js
@@ -2,6 +2,23 @@ require('./runtime')
 
 const RPC = require('tiny-buffer-rpc')
 const ce = require('compact-encoding')
+try{
+  console.log(Bare.arch, 'Bare.arch')
+  const fs = require('bare-fs')
+
+  const readFileRPC = () => {
+    fs.readFile('file.txt', 'utf8', (err, data) => {
+      if (err) {
+          console.error('Error reading file:', err);
+          return 'Error';
+      }
+      return data
+    });
+  }
+
+}catch(e){
+  console.log('Error: ', e)
+}
 
 const rpc = new RPC(HelloBare.sendMessage)
 HelloBare.onMessage = rpc.recv.bind(rpc)
@@ -10,6 +27,7 @@ rpc.register(0, {
   request: ce.string,
   response: ce.string,
   onrequest: message => message.split('').reverse().join('')
+  // onrequest: message => readFileRPC()
 })
 
 // keep the event loop alive
